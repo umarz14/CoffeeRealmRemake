691c0be4a5f524562a6187dc083e1988774d09f2
umarz14 <martinezuriel2016@gmail.com>
2023-09-09 18:16:25 -0700

    homepage 1st part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { CommonModule } from '@angular/common';
-import {HttpClientModule, HttpClientJsonpModule} from '@angular/common/http'
+import { HttpClientModule, HttpClientJsonpModule} from '@angular/common/http'
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { ShopLocationComponent } from './shop-location/shop-location.component';
 import { ShopDetailsComponent } from './shop-details/shop-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -28,6 +30,8 @@ import { ShopDetailsComponent } from './shop-details/shop-details.component';
     CommonModule,
     HttpClientModule,
     HttpClientJsonpModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
