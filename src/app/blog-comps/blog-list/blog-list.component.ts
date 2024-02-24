155fe8796c91570422a71a90a64c6f0ca2cb2c0b
umarz14 <martinezuriel2016@gmail.com>
2024-02-23 19:23:47 -0800

    blog list fetch data from backend

--- src/app/blog-comps/blog-list/blog-list.component.ts
+++ src/app/blog-comps/blog-list/blog-list.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Firestore, collectionData } from '@angular/fire/firestore';
+import { collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
+import { Blog } from 'src/app/models/blog.model';
 
 @Component({
   selector: 'app-blog-list',
@@ -6,5 +11,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog-list.component.css']
 })
 export class BlogListComponent {
-
+  blogs$: Observable<Blog[]>;
+  constructor( private firestore: Firestore ) {
+    const blogCollection = collection(this.firestore, 'blogs');
+    this.blogs$ = collectionData(blogCollection) as Observable<Blog[]>;
+  }
 }
---
+++ src/app/models/blog.model.ts
@@ -0,0 +1,12 @@
+export interface Blog {
+    blogId: string,
+    creatorId: string,
+    creator: string, 
+    title: string,
+    content: string,
+    datePublished: Date
+    photoUrl: string, 
+    // author: string,
+    // text: string,
+    // title: string,
+}
