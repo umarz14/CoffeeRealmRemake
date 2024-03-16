a8047a82920899e39b175eb3dea7beda992f978c
umarz14 <martinezuriel2016@gmail.com>
2024-03-16 09:33:29 -0700

    small navabar change

--- src/app/blog-comps/blog-list/blog-list.component.ts
+++ src/app/blog-comps/blog-list/blog-list.component.ts
@@ -4,6 +4,7 @@ import { collection } from 'firebase/firestore';
 import { Observable } from 'rxjs';
 
 import { Blog } from 'src/app/models/blog.model';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-blog-list',
@@ -12,7 +13,8 @@ import { Blog } from 'src/app/models/blog.model';
 })
 export class BlogListComponent {
   blogs$: Observable<Blog[]>;
-  constructor( private firestore: Firestore ) {
+
+  constructor( public authService: AuthService, private firestore: Firestore ) {
     const blogCollection = collection(this.firestore, 'blogs');
     this.blogs$ = collectionData(blogCollection) as Observable<Blog[]>;
   }
