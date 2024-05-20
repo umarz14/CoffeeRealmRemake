b30219649c3e174c3ed985612668080d14b1c9a7
umarz14 <martinezuriel2016@gmail.com>
2024-05-19 18:36:26 -0700

    set up environment variables

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    MY_GOOGLE_MAPS_JS_API_KEY: process.env["APP_GOOGLE_MAPS_JS_API_KEY"] || '',
+    firebaseConfig: {
+        apiKey: process.env["APP_FIREBASE_API_KEY"] || '',
+        authDomain: "coffee-realm.firebaseapp.com",
+        projectId: "coffee-realm",
+        storageBucket: "coffee-realm.appspot.com",
+        messagingSenderId: "112481585467",
+        appId: "1:112481585467:web:2fdacf93a3653218960f1e",
+        measurementId: "G-S504NEPG0N"
+    },
+};
