18f6df335d4c44fd5d8cb4886b4506e3aec0eff6
umarz14 <martinezuriel2016@gmail.com>
2024-05-19 21:27:02 -0700

    update workflows, env, and ignore

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    MY_GOOGLE_MAPS_JS_API_KEY: process.env["NG_APP_GOOGLE_MAPS_JS_API_KEY"] || '',
+    firebaseConfig: {
+        apiKey: process.env["NG_APP_FIREBASE_API_KEY"] || '',
+        authDomain: "coffee-realm.firebaseapp.com",
+        projectId: "coffee-realm",
+        storageBucket: "coffee-realm.appspot.com",
+        messagingSenderId: "112481585467",
+        appId: "1:112481585467:web:2fdacf93a3653218960f1e",
+        measurementId: "G-S504NEPG0N"
+    },
+};
+  
