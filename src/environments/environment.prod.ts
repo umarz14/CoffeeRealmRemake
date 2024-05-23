5385cb0cb2e220df2b12e63db586a04ceb8665c1
umarz14 <martinezuriel2016@gmail.com>
2024-05-22 17:24:04 -0700

    fix typo in env.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    MY_GOOGLE_MAPS_JS_API_KEY: process.env['MY_GOOGLE_API'] || '',
+    MY_GOOGLE_MAPS_JS_API_KEY: process.env['MY_GOOGLE_API_KEY'] || '',
     firebaseConfig: {
         apiKey: process.env['FIREBASE_API_KEY'] || '',
         authDomain: "coffee-realm.firebaseapp.com",
