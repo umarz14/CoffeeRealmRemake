b4eb29b484bc80940e9c4e69240c64129573a73b
umarz14 <martinezuriel2016@gmail.com>
2024-05-22 19:41:43 -0700

    api key injection attempt

--- src/app/services/api-loader/script-loader.service.ts
+++ src/app/services/api-loader/script-loader.service.ts
@@ -6,11 +6,13 @@ import { environment } from 'src/environments/environment';
 })
 export class ScriptLoaderService {
 
+  private GOOGLE_API_KEY = environment.MY_GOOGLE_MAPS_JS_API_KEY;
+
   constructor() { }
 
   async addGoogleMapsApi() {
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.MY_GOOGLE_MAPS_JS_API_KEY}&loading=async&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${this.GOOGLE_API_KEY}&loading=async&libraries=places`;
     script.async = true;
     script.defer = true;
     document.body.appendChild(script);
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    MY_GOOGLE_MAPS_JS_API_KEY:'',
+    MY_GOOGLE_MAPS_JS_API_KEY: process.env["MY_GOOGLE_MAPS_JS_API_KEY"],
     firebaseConfig: {
-        FIREBASE_API_KEY:'',
+        FIREBASE_API_KEY: process.env["FIREBASE_API_KEY"],
         authDomain: "coffee-realm.firebaseapp.com",
         projectId: "coffee-realm",
         storageBucket: "coffee-realm.appspot.com",
