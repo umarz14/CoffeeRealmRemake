91c7a250444f2313f6fb4a6c6a611f63a3cbf02d
umarz14 <martinezuriel2016@gmail.com>
2024-05-22 19:50:50 -0700

    inject api key into build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    MY_GOOGLE_MAPS_JS_API_KEY: process.env["MY_GOOGLE_MAPS_JS_API_KEY"],
+    // MY_GOOGLE_MAPS_JS_API_KEY: process.env["MY_GOOGLE_MAPS_JS_API_KEY"],
     firebaseConfig: {
         FIREBASE_API_KEY: process.env["FIREBASE_API_KEY"],
         authDomain: "coffee-realm.firebaseapp.com",
