87c755be33f6160b21077e96065749be7d4ca905
umarz14 <martinezuriel2016@gmail.com>
2024-05-22 20:11:44 -0700

    another attempt at injecting api keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
-    // MY_GOOGLE_MAPS_JS_API_KEY: process.env["MY_GOOGLE_MAPS_JS_API_KEY"],
+     MY_GOOGLE_MAPS_JS_API_KEY: '', // attempting to dynamically load this key from actions
     firebaseConfig: {
-        FIREBASE_API_KEY: process.env["FIREBASE_API_KEY"],
+        FIREBASE_API_KEY: '', // attempting to dynamically load this key from actions
         authDomain: "coffee-realm.firebaseapp.com",
         projectId: "coffee-realm",
         storageBucket: "coffee-realm.appspot.com",
@@ -11,3 +11,18 @@ export const environment = {
         measurementId: "G-S504NEPG0N"
     },
 };
+
+// export const environment = {
+//     production: true,
+//      MY_API_KEY: 'some key',
+//      firebaseConfig: {
+//         FIREBASE_API_KEY: 'some key',
+//         authDomain: "angularapp.firebaseapp.com",
+//         projectId: "angularapp",
+//         storageBucket: "angularapp.appspot.com",
+//         messagingSenderId: "11221385467",
+//         appId: "1:112481585467:web:2fdacf93a36148395460f1e",
+//         measurementId: "G-S200NEPG1N"
+//     },
+// };
+
